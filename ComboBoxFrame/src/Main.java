8aa65042a832917f4126481c0f8002b03f587a22
RafaelEnzo3b <158351142+RafaelEnzo3b@users.noreply.github.com>
2024-03-20 19:00:52 -0300

    Add files via upload

---
+++ ComboBoxFrame/src/Main.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+public class Main {
+    public class ComboBoxTest{
+        public static void main (String[] args){
+            comboBoxFrame comboBoxFrame1 = new ComboBoxFrame();
+            comboBoxFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE);
+            comboBoxFrame.setSize( 350, 150);
+            comboBoxFrame.setVisible( true );
+
+        }
+    }
+}
